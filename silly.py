33554deb2c996f7efd2659c1c29f6061917a31bd
lidapar <102316866+lidapar@users.noreply.github.com>
2022-03-30 14:09:27 +0700

    Create silly.py

---
+++ silly.py
@@ -0,0 +1,28 @@
+# https://codehs.com/sandbox/id/python-3-FJ9Q5s?filename=main.py
+
+import asyncio
+from pyppeteer import launch
+import time
+
+async def main():
+    browser = await launch(options={'args': ['--no-sandbox'], 'devtools':True, 'headless':True})
+    page = await browser.newPage()
+    await page.goto('https://codehs.com/sandbox/id/python-3-FuF5AE?filename=main.py')
+    await page.screenshot({'path': './code.png'})
+    time.sleep(2)
+    # await page.screenshot({'path': './code.png'})
+    for x in range(3000):
+        await asyncio.sleep(0.5)
+        await page.screenshot({'path': "r1.png"})
+        await asyncio.sleep(10)
+        await page.keyboard.press('KeyH')
+        await asyncio.sleep(10)
+        await page.keyboard.press('KeyH')
+        await asyncio.sleep(10)
+        await page.keyboard.press('KeyH')
+        await asyncio.sleep(70)
+        await page.mouse.click(580,125,{'button': 'left'})
+        await page.screenshot({'path': "res.png"})
+
+    await browser.close()
+asyncio.get_event_loop().run_until_complete(main())
