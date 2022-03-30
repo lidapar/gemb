399e4cf2f74d2658f9b2fef9d023fb2f66113db8
lidapar <102316866+lidapar@users.noreply.github.com>
2022-03-30 14:09:02 +0700

    Create silly.js

---
+++ silly.js
@@ -0,0 +1,31 @@
+const userAgent = require('user-agents');
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox']
+ });
+
+  const page = await browser.newPage();
+  await page.setUserAgent(userAgent.toString())
+  await page.goto('https://codehs.com/sandbox/id/python-3-jLlrsf?filename=ab.py')
+  await page.screenshot({'path': './codejs.png'})
+  await page.waitForTimeout(2000)
+//   # await page.screenshot({'path': './code.png'})
+ for(x=0;x<10000;x++) {
+    await page.waitForTimeout(500)
+    // await page.screenshot({'path': "r1.png"})
+    await page.waitForTimeout(10000)
+    await page.keyboard.press('KeyH')
+    await page.waitForTimeout(10000)
+    await page.keyboard.press('KeyH')
+    await page.screenshot({'path': "r1.png"})
+    await page.waitForTimeout(10000)
+    await page.keyboard.press('KeyH')
+    await page.waitForTimeout(70000)
+    await page.mouse.click(580,125,{'button': 'left'})
+    await page.screenshot({'path': "resjs.png"})
+ }
+  await browser.close();
+})();
