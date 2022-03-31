70cd5d6035d82eb9757bbcedfc6fcfeeefd48020
lidapar <102316866+lidapar@users.noreply.github.com>
2022-03-31 22:59:39 +0700

    Update silly.js

--- silly.js
+++ silly.js
@@ -8,24 +8,30 @@ const puppeteer = require('puppeteer');
  });
 
   const page = await browser.newPage();
-  await page.setUserAgent(userAgent.toString())
-  await page.goto('https://codehs.com/sandbox/id/python-3-FuF5AE')
-//   await page.screenshot({'path': './codejs.png'})
+  // await page.setUserAgent(userAgent.toString())
+  await page.goto('https://codehs.com/sandbox/id/python-3-oaPgUx')
+  // await page.screenshot({'path': './codejs.png'})
   await page.waitForTimeout(2000)
 //   # await page.screenshot({'path': './code.png'})
+  await page.mouse.click(580,125,{'button': 'left'})
  for(x=0;x<10000;x++) {
+   for(y=0;y<37;y++) { //37
     await page.waitForTimeout(500)
     // await page.screenshot({'path': "r1.png"})
     await page.waitForTimeout(10000)
     await page.keyboard.press('KeyH')
     await page.waitForTimeout(10000)
     await page.keyboard.press('KeyH')
-//     await page.screenshot({'path': "r1.png"})
     await page.waitForTimeout(10000)
     await page.keyboard.press('KeyH')
-    await page.waitForTimeout(10000)
-//     await page.mouse.click(580,125,{'button': 'left'})
-    await page.screenshot({'path': "resjs.png"})
+    console.log(`pimpo${x}`)
+    await page.mouse.click(580,125,{'button': 'left'})
+    // await page.screenshot({'path': `resjs${x}.png`})
+   }
+   // stop 
+   await page.mouse.click(620,125,{'button': 'left'})
+   await page.waitForTimeout(1000)
+  //  await page.screenshot({'path': "stop.png"})
  }
   await browser.close();
 })();
